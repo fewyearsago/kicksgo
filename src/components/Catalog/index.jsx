e20ed3e7d964a109c154216c62273ae953aa17f4
Muhammad Zaurbekov <fewyearsago@mail.ru>
2023-05-21 00:49:29 +0300

    saasdasd

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -48,18 +48,20 @@ const Catalog = () => {
   }, [currentPage, sortType]);
 
   return (
-    <div className={style.root}>
-      <div className={style.container}>
-        <div className={style.rootCatalogWrapper}>
-          <h1 className={style.rootTitle}>Каталог ❤️</h1>
-          <Sort sortTitle={sortType} onChangeSort={(i) => setSortType(i)} />
+    <section id="section1">
+      <div className={style.root}>
+        <div className={style.container}>
+          <div className={style.rootCatalogWrapper}>
+            <h1 className={style.rootTitle}>Каталог ❤️</h1>
+            <Sort sortTitle={sortType} onChangeSort={(i) => setSortType(i)} />
+          </div>
+          <div className={style.rootCatalogList}>
+            {isLoading ? ShortSkeleton : ShortItemBlock}
+          </div>
+          <Pagination onChangePage={(number) => setCurrentPage(number)} />
         </div>
-        <div className={style.rootCatalogList}>
-          {isLoading ? ShortSkeleton : ShortItemBlock}
-        </div>
-        <Pagination onChangePage={(number) => setCurrentPage(number)} />
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/ForHimAndHer/index.jsx
+++ src/components/ForHimAndHer/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './index.module.scss';
 import ImgForMen from '../../../public/1.png';
 import ImgForWomen from '../../../public/2.png';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 const ForHimAndHer = () => {
   return (
@@ -9,16 +10,30 @@ const ForHimAndHer = () => {
       <div className={style.container}>
         <div className={style.root}>
           <div className={style.rootWomen}>
-            <a href="#">
+            <Link
+              className={style.rootLink}
+              activeClass="active"
+              to="section1"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}>
               <img className={style.rootImg} src={ImgForMen} alt="" />
               <h1 className={style.rootTitle}>Для него</h1>
-            </a>
+            </Link>
           </div>
           <div className={style.rootMen}>
-            <a href="#">
+            <Link
+              className={style.rootLink}
+              activeClass="active"
+              to="section1"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}>
               <img className={style.rootImg} src={ImgForWomen} alt="" />
               <h1 className={style.rootTitle}>Для неё</h1>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
